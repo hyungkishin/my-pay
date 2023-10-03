c031e611f461eb556e05295cb8b2cb98dc9c5bfa
hyungkishin <tkaqkeldk@nate.com>
2023-10-03 14:53:38 +0900

    feat: money Service 구현

---
+++ money-service/src/main/java/com/mypay/money/adapter/in/web/MoneyChangingResultDetailMapper.java
@@ -0,0 +1,11 @@
+package com.mypay.money.adapter.in.web;
+import com.mypay.money.domain.MoneyChangingRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MoneyChangingResultDetailMapper {
+    public MoneyChangingResultDetail mapToMoneyChangingResultDetail(MoneyChangingRequest moneyChangingRequest) {
+
+      return null;
+    }
+}
